9b9270fba3477f23c4039a2467c86a235eb42da3
Darshan H <darshan8983@gmail.com>
2022-09-07 15:59:29 +0530

    logging created

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -4,6 +4,11 @@ import pandas as pd
 import os
 import shutil # this package to copy the data from one location to other
 from tqdm import tqdm # this package gives a progress bar
+import logging
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,filemode="a")
 
 def copy_file(source_download_dir, local_data_dir):
     list_of_files = os.listdir(source_download_dir)
@@ -30,4 +35,10 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    get_data(config_path=parsed_args.config)
+    try:
+        logging.info(">>>>> stage one started")
+        get_data(config_path=parsed_args.config)
+        logging.info("stage one completed! all the data are saved in local >>>>>\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
